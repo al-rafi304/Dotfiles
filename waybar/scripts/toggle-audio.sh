f7e50f5fe3489713fede0a97273ea84314492768
Mohammad Al Rafi <arafi304@gmail.com>
2026-04-05 14:10:49 +0600

    refactor: change sink source for headphone

--- waybar/scripts/toggle-audio.sh
+++ waybar/scripts/toggle-audio.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Define your sinks by name
-HEADPHONE="alsa_output.usb-GeneralPlus_USB_Audio_Device-00.analog-stereo"
+HEADPHONE="alsa_output.usb-C-Media_Electronics_Inc._EKSA_E900_Pro-00.analog-stereo"
 SPEAKER="alsa_output.pci-0000_00_1f.3.analog-stereo"
 
 # Get current default sink
